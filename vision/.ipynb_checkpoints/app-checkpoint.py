8c5e25512b83257ebe1246efce4f85288a43655c
sinharitesh <sinharitesh@gmail.com>
2021-05-07 12:16:19 +0000

    code-cleanup

--- vision/.ipynb_checkpoints/app-checkpoint.py
+++ vision/.ipynb_checkpoints/app-checkpoint.py
@@ -25,10 +25,6 @@ def download_file_from_s3_bucket(bucket_name = "ecg-png-data", file_name = "s055
 def download_file(url):
     file_name = '/tmp/test_file.png'
     urllib.request.urlretrieve(url, file_name)
-#     mp3file = urllib2.urlopen(url)
-#     with open('/tmp/test_file.png','wb') as output:
-#       output.write(mp3file.read())
-#     output.close
 
 
 def lambda_handler(event, context):
@@ -48,29 +44,12 @@ def lambda_handler(event, context):
     
     if img_fname == "":
         try:
-            print("SECOND TRY:", type(event))
-            #if isinstance(event, dict):
-            #request_body = json.loads(request_body)
             request_body = json.loads(event)
             img_fname = request_body['fname']
         except:
             pass
     
     logger.info(img_fname)
-    print("IMGXXX:", img_fname)
-    #input_object = input_fn(event['body'])
-#     print("Type:", type(event))
-#     fname = ""
-#     if type(event) == dict:
-#         fname = event['fname']
-#     if type(event) == str:
-#         jevent = json.loads(event)
-#         fname =  jevent['fname']
-        
-    #print("FNAME:", jevent['fname'])
-#     print(f"url is: {body['url']}")
-    #fname = body['fname']
-    # download_file(url) Works currently commenting out
     start = time.time()
     fname = img_fname
     download_file_from_s3_bucket(file_name = fname)
@@ -83,7 +62,6 @@ def lambda_handler(event, context):
     except:
         pass
     end = time.time()
-    #print(fname, out[0])
     inference_time = np.round((end - start) * 1000, 2)
     
     message = f'For file:{img_fname}- result is: [{out[0]}] time taken: {str(inference_time)} ms'
@@ -101,30 +79,3 @@ def lambda_handler(event, context):
             }
         ),
     }
-    
-    #---------------------------------------------------------------------------------------------------------#
-    #print("Received event: " + json.dumps(event, indent=2))
-#     body = json.loads(event['body'])
-#     print(f"Body is: {body}")
-#     url = body['url']
-#     print(f"Getting image from URL: {url}")
-#     response = requests.get(url)
-#     print("Load image into memory")
-#     img = PILImage.create(BytesIO(response.content))
-#     print("Doing forward pass")
-#     start = time.time()
-#     pred,pred_idx,probs = learn.predict(img)
-#     end = time.time()
-#     inference_time = np.round((end - start) * 1000, 2)
-#     print(f'class: {pred}, probability: {probs[pred_idx]:.04f}')
-#     print(f'Inference time is: {str(inference_time)} ms')
-#     return {
-#         "statusCode": 200,
-#         "body": json.dumps(
-#             {
-#                 "class": pred,
-#                 "probability": "%.4f" % probs[pred_idx]
-#             }
-#         ),
-#     }
-
